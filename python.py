9bf42273da3a1ae55c17d3f8cee49c16553a33b4
Farhana <farhanna700@gmail.com>
2023-07-18 12:57:35 +0100

    python

---
+++ python.py
@@ -0,0 +1,9 @@
+books = {'John Green':'Paper towns', 'Harper lee':'To kill a mockingbird', 'Louisa may alcott':'Little women'}
+
+authorname = str(input('Enter the authors full name: " "'))
+
+print("".join(books[authorname]))
+
+
+
+

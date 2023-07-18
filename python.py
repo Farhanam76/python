fbc7b47c53266d19d8cf57a6a2b37daab0274925
Farhanam76 <138291154+Farhanam76@users.noreply.github.com>
2023-07-18 14:29:40 +0100

    Update python.py

--- python.py
+++ python.py
@@ -4,6 +4,31 @@ authorname = str(input('Enter the authors full name: " "'))
 
 print("".join(books[authorname]))
 
+#elif and if exercise 
+mark = int(input('enter your mark'))
+
+if mark > 85:
+    print('distinction')
+elif mark == 85:
+    print('pass')
+elif mark >= 65:
+    print('pass')
+else:
+    print('fail')
+
+#without elif 
+mark = int(input('enter your mark'))
+
+if mark > 85:
+    print('distinction')
+if mark == 85:
+    print('pass')
+if mark >= 65 and mark < 85:
+    print('pass')
+if mark < 65:
+    print('fail')
+
+
 
 
 
